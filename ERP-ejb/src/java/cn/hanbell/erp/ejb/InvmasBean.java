02e0ed337d9792a02d94804b9a419e428b20b1f8
C1491 <zhoufeifei@hanbell.cn>
2016-10-12 09:20:49 +0800

    件号修改补充

--- ERP-ejb/src/java/cn/hanbell/erp/ejb/InvmasBean.java
+++ ERP-ejb/src/java/cn/hanbell/erp/ejb/InvmasBean.java
@@ -11,10 +11,14 @@ import cn.hanbell.oa.ejb.HZJS034Bean;
 import cn.hanbell.oa.ejb.HZJS034DetailBean;
 import cn.hanbell.oa.ejb.SHBINV140Bean;
 import cn.hanbell.oa.ejb.SHBINV140DetailBean;
+import cn.hanbell.oa.ejb.SHBINV146Bean;
+import cn.hanbell.oa.ejb.SHBINV146DetailBean;
 import cn.hanbell.oa.entity.HZJS034;
 import cn.hanbell.oa.entity.HZJS034Detail;
 import cn.hanbell.oa.entity.SHBERPINV140Detail;
 import cn.hanbell.oa.entity.SHBERPINV140;
+import cn.hanbell.oa.entity.SHBERPINV146;
+import cn.hanbell.oa.entity.SHBERPINV146Detail;
 import cn.hanbell.util.BaseLib;
 import java.math.BigDecimal;
 import java.util.List;
@@ -38,6 +42,10 @@ public class InvmasBean extends SuperEJBForERP<Invmas> {
     private SHBINV140Bean shbinv140Bean;
     @EJB
     private SHBINV140DetailBean shbinv140DetailBean;
+    @EJB
+    private SHBINV146Bean shbinv146Bean ;
+    @EJB
+    private SHBINV146DetailBean shbinv146DetailBean ;
 
     public InvmasBean() {
         super(Invmas.class);
@@ -52,7 +60,7 @@ public class InvmasBean extends SuperEJBForERP<Invmas> {
             return null;
         }
     }
-
+/*
     public Boolean initByOAJHSQD(String psn) {
 
         HZJS034 h = hzjs034Bean.findByPSN(psn);
@@ -66,19 +74,31 @@ public class InvmasBean extends SuperEJBForERP<Invmas> {
                 Invmas m = new Invmas();
                 m.setItnbr(detail.getItnbr());       // 设置件号
                 m.setItcls(detail.getItcls());       // 设置品号大类  
+                
                 m.setItclscode('3');                //
+                
                 m.setItdsc(detail.getItdsc());      //设置中文品名
                 m.setSpdsc(detail.getSpdsc());      //设置中文规格
-                m.setEitdsc(detail.getEitdsc());
-                m.setEspdsc(detail.getEitdsc());    //设置英文品名
+                m.setEitdsc(detail.getEitdsc());    //设置英文品名   
                 m.setEspdsc(detail.getEspdsc());    //设置英文规格
                 m.setUnmsr1(detail.getUnmsr1());    //设置单位一
                 m.setUnmsr2(detail.getUnmsr2());    //设置单位二
+                
+                m.setUnmsr1e(psn);                  //设置数量单位一（英文）
+                
+                m.setFvco('F');                     //设置固定变动区分码
+                m.setJudco("11111");                //设置数量单位控制码
                 m.setMorpcode(detail.getMorpcode());
-                m.setFvco('F');
+                
+                m.setGroup1(psn);                    //设置分类码（一） ;
+                m.setGroup2(psn);                    //设置分类码（二） ；
+                
+                m.setDirrvyn(Character.MIN_VALUE);    //设置知否为直接验收
+                
                 m.setIndate(BaseLib.getDate());
 
                 persist(m);
+                this.getEntityManager().flush();
 
             }
 
@@ -88,7 +108,8 @@ public class InvmasBean extends SuperEJBForERP<Invmas> {
             return false;
         }
     }
-
+*/
+    
     //件号转移申请单
     public Boolean initByOASHBINV140(String psn) {
 
@@ -126,6 +147,43 @@ public class InvmasBean extends SuperEJBForERP<Invmas> {
                 this.getEntityManager().detach(m);
                 setCompany(h.getFacno2());
                 persist(m);
+                this.getEntityManager().flush();
+            }
+
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(InvmasBean.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+      
+    //件号名称规格修改单
+    public Boolean updateByOASHBINV146(String psn) {
+
+        SHBERPINV146 h = shbinv146Bean.findByPSN(psn);
+        List<SHBERPINV146Detail> details = shbinv146DetailBean.findByFSN(h.getFormSerialNumber());
+
+        try {
+
+            //  表身循环
+            for (int i = 0; i < details.size(); i++) {
+
+                SHBERPINV146Detail detail = details.get(i);
+
+                this.setCompany(h.getFacno());
+                Invmas m = findByItnbr(detail.getItnbr());
+                //m.setItcls(detail.getItcls());                                  //设置品号大类               
+                m.setItdsc(detail.getItdsc());                                  //设置中文品名
+                m.setSpdsc(detail.getSpdsc());                                  //设置中文规格
+                m.setEitdsc(detail.getEitdsc());                                //设置英文品名
+                m.setEspdsc(detail.getEspdsc());                                //设置英文规格
+                m.setSitdsc(detail.getSitdsc());                                //设置品号简名
+                m.setItdsc2(detail.getItdsc2());
+                m.setSpdsc2(detail.getSpdsc2());
+                m.setIndate(BaseLib.getDate());
+                m.setUserno(h.getApplyuser());
+               
+                update(m);
                 this.getEntityManager().flush();
             }
 
