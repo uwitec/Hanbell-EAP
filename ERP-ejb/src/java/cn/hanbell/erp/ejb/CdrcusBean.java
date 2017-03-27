0935a871f8d1d81692c32ad7debd19ab3e46ca0c
C1491 <zhoufeifei@hanbell.cn>
2017-03-27 13:05:05 +0800

    采购抛转号修改，客户地址修改
    采购抛转号修改，客户地址修改

--- ERP-ejb/src/java/cn/hanbell/erp/ejb/CdrcusBean.java
+++ ERP-ejb/src/java/cn/hanbell/erp/ejb/CdrcusBean.java
@@ -164,7 +164,9 @@ public class CdrcusBean extends SuperEJBForERP<Cdrcus> {
             cdrcus.setAddress1(oa.getGg036());
         } else if (oa.getGg036().length() > 40) {
             String str = oa.getGg036();
-            String str1 = oa.getGg036().substring(0, 40);
+            str = str.replace('(', '（');
+            str = str.replace(')', '）');
+            String str1 = str.substring(0, 40);
             String[] split = str.split(str1);
             cdrcus.setAddress1(str1);
             cdrcus.setAddress2(split[1]);
