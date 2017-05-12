83fc152e897970df40a8e51ee4a64d0509c52841
DongTianyu <dtianyu@hanbell.cn>
2017-05-12 15:24:16 +0800

    Hanbell-Admin增加公司信息维护功能
    组织架构和人员统一到Hanbell-Admin下管理，将来EAP子项目只需调用

---
+++ EAP-ejb/src/java/cn/hanbell/eap/ejb/CompanyBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.eap.ejb;
+
+
+import cn.hanbell.eap.comm.SuperEJBForEAP;
+import cn.hanbell.eap.entity.Company;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author C0160
+ */
+@Stateless
+@LocalBean
+public class CompanyBean extends SuperEJBForEAP<Company> {
+
+    public CompanyBean() {
+        super(Company.class);
+    }
+
+    public Company findByCompany(String value) {
+        Query query = getEntityManager().createNamedQuery("Company.findByCompany");
+        query.setParameter("company", value);
+        try {
+            Object o = query.getSingleResult();
+            return (Company) o;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+}
