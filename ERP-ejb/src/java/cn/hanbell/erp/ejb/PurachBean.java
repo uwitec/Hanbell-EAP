0e2ebb2f9f4075d52e96f7a497572a6de913d6ed
DongTianyu <dtianyu@hanbell.cn>
2017-06-23 13:44:44 +0800

    syncEAMWithERP

---
+++ ERP-ejb/src/java/cn/hanbell/erp/ejb/PurachBean.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.erp.ejb;
+
+import cn.hanbell.erp.comm.SuperEJBForERP;
+import cn.hanbell.erp.entity.Puracd;
+import cn.hanbell.erp.entity.Purach;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author C0160
+ */
+@Stateless
+@LocalBean
+public class PurachBean extends SuperEJBForERP<Purach> {
+
+    @EJB
+    private PuracdBean puracdBean;
+
+    private List<Puracd> detailList;
+
+    public PurachBean() {
+        super(Purach.class);
+    }
+
+    public Purach findByAcceptno(String value) {
+        Query query = getEntityManager().createNamedQuery("Purach.findByAcceptno");
+        query.setParameter("acceptno", value);
+        try {
+            Object o = query.getSingleResult();
+            return (Purach) o;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public List<Puracd> findByAcceptnoAndItnbr(String acceptno, String itnbr) {
+        return puracdBean.findByAcceptnoAndItnbr(acceptno, itnbr);
+    }
+
+    public String[] findByPRN(String prno) {
+        String[] acceptno = null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("SELECT DISTINCT acceptno FROM purdtamap,puracd WHERE purdtamap.facno=puracd.facno AND purdtamap.prono=puracd.prono AND purdtamap.pono=puracd.pono AND purdtamap.trseq =puracd.ponotrseq ");
+        sb.append(" AND srcno=?1");
+        Query query = getEntityManager().createNativeQuery(sb.toString());
+        query.setParameter(1, prno);
+        List result = query.getResultList();
+        if (result != null && !result.isEmpty()) {
+            Object[] row;
+            acceptno = new String[result.size()];
+            for (int i = 0; i < result.size(); i++) {
+                row = (Object[]) result.get(i);
+                acceptno[i] = row[0].toString();
+            }
+        }
+        return acceptno;
+    }
+
+    @Override
+    public void setDetail(Object value) {
+        detailList = puracdBean.findByAcceptno(value.toString());
+    }
+
+    /**
+     * @return the detailList
+     */
+    public List<Puracd> getDetailList() {
+        return detailList;
+    }
+
+}
