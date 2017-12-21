ab6a59e3b9bebc931c4a7449bd277e3fda06e628
C1587 <fankangjie@hanbell.cn>
2017-12-21 09:53:39 +0800

    越南借支归还单还款金额处理
    越南借支归还单还款金额处理

---
+++ EFGP-ejb/src/java/cn/hanbell/oa/ejb/VHTV002reDetailBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.hanbell.oa.ejb;
+
+import cn.hanbell.oa.comm.SuperEJBForEFGP;
+import cn.hanbell.oa.entity.VHTV002reDetail;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Administrator
+ */
+@Stateless
+@LocalBean
+public class VHTV002reDetailBean extends SuperEJBForEFGP<VHTV002reDetail>{
+    
+
+
+    public VHTV002reDetailBean() {
+        super(VHTV002reDetail.class);
+    }
+}
